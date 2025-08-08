e353885136e9ac597303aa4b9778596108990313
caue <caue.uanus@cbm.se.gov.br>
2025-08-08 01:08:11 -0300

    Traduzindo categorias

--- src/resources/views/categories/index.blade.php
+++ src/resources/views/categories/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-sm-6">
                     <a class="btn btn-primary float-right"
                        href="{{ route('categories.create') }}">
-                        Adiconar Nova Categoria
+                        Adiconar Categoria
                     </a>
                 </div>
             </div>
